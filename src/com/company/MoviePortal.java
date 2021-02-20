f771f0f44210371e27ebbe559f2a527342d9a351
Marina Neumann <marinaneumann2468@yahoo.com>
2021-02-20 15:36:41 -0800

    Added README to better outline goals and use cases to be designed

--- src/com/company/MoviePortal.java
+++ src/com/company/MoviePortal.java
@@ -1,9 +1,29 @@
 package com.company;
+import java.util.*;
 
 public class MoviePortal {
     public static void main(String[] args) {
         // write your code here
-        System.out.println("What?");
+        Movie[] Portal = new Movie[100];
+//        System.out.println("What?");
         Movie m = new Movie();
     }
+
+//    public void menu(){
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("Enter 'a' to add a new movie to the database");
+//        System.out.println("Enter 'd' to display a movie in the database");
+//        System.out.println("Enter 's' to search the portal for a movie:");
+//        System.out.println("Enter 'e' to exit this program");
+//        String choice = in.next();
+//        if(choice == "a"){
+//            add();
+//        }else if (choice == "d"){
+//            //display();
+//        }else if (choice == "s"){
+//            //search();
+//        };
+//    }
+
+
 }
---
+++ src/com/company/README.md
@@ -0,0 +1,19 @@
+Movie Portal Project 
+By Marina Neumann 
+2021 
+
+Goals: 
+A database of movies to track progress on movie bucket list. 
+
+Use a database to store records. 
+Use a movie API to have access to more data. 
+Have good infrastructure for portal to expand or shrink. 
+Have a front-end user-friendly interface. 
+Be able to find all available sources to watch a specific movie (ie. Amazon, owned Dvd, netflix, etc)
+
+
+
+Use Cases: 
+To be able to search for all old movies to watch on your list (ie. movies before 1980). 
+Randomly select movie based on potential search criteria (ie. I want to watch a comedy movie tonight). 
+
